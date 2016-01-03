21dbeff11dbb0b3be65ec19085eb441bfecf2fd7
Kaz <dennenn@gmail.com>
2016-01-03 23:56:29 +0900

    add some.

--- DDL-SAMPLE.sql
+++ DDL-SAMPLE.sql
@@ -1,5 +1,8 @@
 
 CREATE TABLE IF NOT EXISTS example (
-    id INT(11) NOT NULL auto_increment PRIMARY KEY,
-    message TEXT
+    id INT(11) NOT NULL AUTO_INCREMENT COMMENT 'id',
+    email varchar(256) NOT NULL COMMENT 'email',
+    message TEXT DEFAULT 'Hello World' COMMENT 'message',
+    PRIMARY KEY (id),
+    INDEX INDEX_email (email)
 ) DEFAULT CHARSET=utf8
